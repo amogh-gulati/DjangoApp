1021bb7d46fa2018ea3423f6ef784e5b090f750c
amogh-gulati <amogh7777@gmail.com>
2019-10-30 11:41:44 +0530

    poll app with center page

--- pollApp/polls/views.py
+++ pollApp/polls/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
 from .models import Question, Choice
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404,render
+from django.urls import reverse
 # Create your views here.
 #get questions and display items
 #the below url is being requested
